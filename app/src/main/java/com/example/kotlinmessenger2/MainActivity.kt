6952b2b9478c92f2a2f585249263783991c81fcf
MkItSimple <choanania1@gmail.com>
2020-04-25 11:59:27 +0800

    02-FirebaseAuthandRegisterUser

--- app/src/main/java/com/example/kotlinmessenger2/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger2/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.example.kotlinmessenger2.util.toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +25,16 @@ class MainActivity : AppCompatActivity() {
 
             Log.d("MainActivity", "Email is: " + email)
             Log.d("MainActivity", "Password is: " + password)
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (!it.isSuccessful) return@addOnCompleteListener
+                    Log.d(TAG, "Successfully created user with uid: ${it.result?.user?.uid}")
+                }
+                .addOnFailureListener{
+                    Log.d(TAG, "Failed to create user: ${it.message}")
+                    toast("Failed to create user: ${it.message}")
+                }
         }
 
         already_have_account_text_view.setOnClickListener {
